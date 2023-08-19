115e52134bfa471b1c16449c4c9e2666770c9371
Milimas <a.x.man@hotmail.fr>
2023-08-19 18:36:16 +0100

    arm arch

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include "includes/minirt.h"
+
+#ifdef __linux
 #include <mlx.h>
+#endif
+#include "mlx/mlx.h"
 
 int main()
 {
 	void	*mlx;
 	void	*mlx_win;
 
+	(void)mlx_win;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
 
